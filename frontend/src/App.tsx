09210640d49f25095b86348f9577f354c52782de
Amjuz <137517288+amjuz@users.noreply.github.com>
2024-05-15 07:29:09 +0530

    minor changes in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
-import { Routes } from "react-router-dom"
+import { Routes, BrowserRouter, Route } from "react-router-dom"
+import { SignUp } from "./pages/SignUp"
+import { Home } from "./pages/Home"
+import { Blog } from "./pages/Blog"
 
 function App() {
 
   return (
     <>
-      <Routes>
-          
-      </Routes>   
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp/>}/>
+          <Route path="/signin" element={<SignUp/>}/>
+          <Route path="/blog" element={<Blog/>}/>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
